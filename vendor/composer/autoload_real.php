9f1f4aad164e85e7c78df089036e3447e30f37a5
Dennis Ploetner <re@lloc.de>
2025-09-21 15:27:51 +0200

    Add vendor

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInit4e8c6cf25880d0321c51dda4853f9ef2
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit4e8c6cf25880d0321c51dda4853f9ef2::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
