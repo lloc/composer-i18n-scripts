7affd86d7f717393ad5113508428cfc6f7c7f9e6
Dennis Ploetner <re@lloc.de>
2025-09-21 15:23:15 +0200

    Add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lloc/composer-i18n-scripts',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b17c0d9f60de7fcdc8e4709918863b8d7310b3bd',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'lloc/composer-i18n-scripts' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b17c0d9f60de7fcdc8e4709918863b8d7310b3bd',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
